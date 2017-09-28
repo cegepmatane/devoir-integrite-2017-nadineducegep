dbd2dbd899c10cd389a1e6db65e1df2c8d6d2b29
nadineducegep <st-amand.nadine@cgmatane.qc.ca>
2017-09-28 19:51:32 -0400

    lireFormulaire dans la vue de ajouter troupeau

--- src/com/fermedelest/appinventaire/vue/FormulaireAjouterTroupeauVue.java
+++ src/com/fermedelest/appinventaire/vue/FormulaireAjouterTroupeauVue.java
@@ -1,5 +1,7 @@
 package com.fermedelest.appinventaire.vue;
 
+import com.fermedelest.appinventaire.modele.Troupeau;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -8,19 +10,33 @@ import javafx.scene.layout.VBox;
 
 public class FormulaireAjouterTroupeauVue extends VBox 
 {
+	
+	protected TextField champsNom = null;
+	protected TextField champsEcurie = null;
+	
 	public FormulaireAjouterTroupeauVue()
 	{
+		this.champsNom = new TextField();
 		HBox ligneNom = new HBox();
 		ligneNom.getChildren().add(new Label("Nom"));
-		ligneNom.getChildren().add(new TextField());
+		ligneNom.getChildren().add(this.champsNom);
 		this.getChildren().add(ligneNom);
 		
+		this.champsEcurie = new TextField();
 		HBox ligneEcurie = new HBox();
 		ligneEcurie.getChildren().add(new Label("Ecurie"));
-		ligneEcurie.getChildren().add(new TextField());
+		ligneEcurie.getChildren().add(this.champsEcurie);
 		this.getChildren().add(ligneEcurie);
 		
 		this.getChildren().add(new Button("Enregistrer"));
-		
+	}
+	
+	public Troupeau lireFormulaire()
+	{
+		String nom = this.champsNom.getText();
+		String ecurie = this.champsEcurie.getText();
+		Troupeau troupeau = new Troupeau(nom);
+		troupeau.setEcurie(ecurie);
+		return troupeau;
 	}
 }
